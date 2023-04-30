02eddf68c6e69aae9a5dad7874ad8515abb21ccd
Mind-thatsall <mathieurossi77@gmail.com>
2023-04-30 15:19:32 +0200

    Day 2 done with golang

--- GO/main.go
+++ GO/main.go
@@ -1,8 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("test")
+	f, err := os.Open("day2.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	defer f.Close()
+
+	sc := bufio.NewScanner(f)
+
+	scorePartOne := 0
+	scorePartTwo := 0
+	gameOutComePartOne := map[string]int{
+		"A X": 4,
+		"A Y": 8,
+		"A Z": 3,
+		"B X": 1,
+		"B Y": 5,
+		"B Z": 9,
+		"C X": 7,
+		"C Y": 2,
+		"C Z": 6,
+	}
+
+	gameOutComePartTwo := map[string]int{
+		"A X": 3,
+		"A Y": 4,
+		"A Z": 8,
+		"B X": 1,
+		"B Y": 5,
+		"B Z": 9,
+		"C X": 2,
+		"C Y": 6,
+		"C Z": 7,
+	}
+
+	for sc.Scan() {
+		scorePartOne += gameOutComePartOne[string(sc.Text())]
+		scorePartTwo += gameOutComePartTwo[string(sc.Text())]
+	}
+	fmt.Println(scorePartOne)
+	fmt.Println(scorePartTwo)
+
+	if err := sc.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
