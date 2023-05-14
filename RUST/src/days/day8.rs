2ec827aa96d513a63d9a3fd44a44d000e8f50b8d
Mind-thatsall <mathieurossi77@gmail.com>
2023-05-14 21:25:44 +0200

    redo of day8

--- RUST/src/days/day8.rs
+++ RUST/src/days/day8.rs
@@ -1,145 +1,99 @@
-use std::fs;
+use std::fs::{self};
 
-fn count_visible_trees(grid: &[Vec<u32>]) -> usize {
-    let max_length = grid.len() - 1;
-    let mut visible_trees: Vec<Vec<bool>> = grid
-        .iter()
-        .enumerate()
-        .map(|(idx, grid_line)| {
-            let line_idx_max_length = grid_line.len() - 1;
-            grid_line
-                .iter()
-                .enumerate()
-                .map(|(line_idx, _)| {
-                    idx == 0
-                        || idx == max_length
-                        || line_idx == 0
-                        || line_idx == line_idx_max_length
-                })
-                .collect()
-        })
-        .collect();
+fn count_visible_trees_refactored(grid: &Vec<Vec<u8>>, height: &usize, width: &usize) -> usize {
+    let mut visible_trees = 0;
 
-    for y in 0..grid.len() {
-        let mut current_tree_size = 0;
-        for x in 0..grid[0].len() {
-            if x == 0 {
-                current_tree_size = grid[y][x];
-            } else if grid[y][x] > current_tree_size {
-                current_tree_size = grid[y][x];
-                visible_trees[y][x] = true;
+    for y in 1..(height - 1) {
+        for x in 1..(width - 1) {
+            let height_tree = grid[y][x];
+
+            if grid[y][0..x].iter().any(|tree| *tree >= height_tree)
+                && grid[y][(x + 1)..].iter().any(|tree| *tree >= height_tree)
+                && grid[0..y].iter().any(|row| row[x] >= height_tree)
+                && grid[(y + 1)..].iter().any(|row| row[x] >= height_tree)
+            {
+                visible_trees += 1;
             }
         }
     }
 
-    for y in 0..grid.len() {
-        let mut current_tree_size = 0;
-        for x in (0..grid[0].len()).rev() {
-            if x == grid.len() - 1 {
-                current_tree_size = grid[y][x];
-            } else if grid[y][x] > current_tree_size {
-                current_tree_size = grid[y][x];
-                visible_trees[y][x] = true;
-            }
-        }
-    }
-
-    for x in 0..grid.len() {
-        let mut current_tree_size = 0;
-        for y in 0..grid[0].len() {
-            if y == 0 {
-                current_tree_size = grid[y][x];
-            } else if grid[y][x] > current_tree_size {
-                current_tree_size = grid[y][x];
-                visible_trees[y][x] = true;
-            }
-        }
-    }
-
-    for x in 0..grid.len() {
-        let mut current_tree_size = 0;
-        for y in (0..grid[0].len()).rev() {
-            if y == grid.len() - 1 {
-                current_tree_size = grid[y][x];
-            } else if grid[y][x] > current_tree_size {
-                current_tree_size = grid[y][x];
-                visible_trees[y][x] = true;
-            }
-        }
-    }
-
-    visible_trees.iter().flatten().filter(|&&val| val).count()
+    width * height - visible_trees
 }
 
-fn best_scenic_view(grid: &[Vec<u32>]) -> usize {
-    let mut highest_score = 0;
+fn scenic_score_refactored(grid: &Vec<Vec<u8>>, height: &usize, width: &usize) -> usize {
+    let mut max_scenic_score = 0;
+    for y in 1..(height - 1) {
+        for x in 1..(width - 1) {
+            let height_tree = grid[y][x];
 
-    let y_max = grid.len();
-    let x_max = grid[0].len();
-    for (y_idx, grid_line) in grid.iter().enumerate() {
-        for (x_idx, grid_height) in grid_line.iter().enumerate() {
-            let mut scores = [0, 0, 0, 0];
+            let mut scenic_score = 1;
 
-            for x_pos in (0..x_idx).rev() {
-                if grid[y_idx][x_pos] < *grid_height {
-                    scores[0] += 1;
-                } else {
-                    scores[0] += 1;
-                    break;
-                }
-            }
+            // grid[y][0..x].iter().rev() => going from the tree to the edge
+            scenic_score *= if let Some((pos, _)) = grid[y][0..x]
+                .iter()
+                .rev()
+                .enumerate()
+                .find(|(_, tree)| **tree >= height_tree)
+            {
+                pos + 1
+            } else {
+                x
+            };
 
-            for x_pos in (x_idx + 1)..x_max {
-                if grid[y_idx][x_pos] < *grid_height {
-                    scores[1] += 1;
-                } else {
-                    scores[1] += 1;
-                    break;
-                }
-            }
+            scenic_score *= if let Some((pos, _)) = grid[y][(x + 1)..]
+                .iter()
+                .enumerate()
+                .find(|(_, tree)| **tree >= height_tree)
+            {
+                pos + 1
+            } else {
+                width - x - 1
+            };
 
-            for y_pos in (0..y_idx).rev() {
-                if grid[y_pos][x_idx] < *grid_height {
-                    scores[2] += 1;
-                } else {
-                    scores[2] += 1;
-                    break;
-                }
-            }
+            scenic_score *= if let Some((pos, _)) = grid[0..y]
+                .iter()
+                .rev()
+                .enumerate()
+                .find(|(_, row)| row[x] >= height_tree)
+            {
+                pos + 1
+            } else {
+                y
+            };
 
-            for y_pos in (y_idx + 1)..y_max {
-                if grid[y_pos][x_idx] < *grid_height {
-                    scores[3] += 1;
-                } else {
-                    scores[3] += 1;
-                    break;
-                }
-            }
+            scenic_score *= if let Some((pos, _)) = grid[(y + 1)..]
+                .iter()
+                .enumerate()
+                .find(|(_, row)| row[x] >= height_tree)
+            {
+                pos + 1
+            } else {
+                height - y - 1
+            };
 
-            let scenic_score = scores.iter().product::<usize>();
-
-            if scenic_score > highest_score {
-                highest_score = scenic_score;
+            if scenic_score > max_scenic_score {
+                max_scenic_score = scenic_score;
             }
         }
     }
 
-    highest_score
+    max_scenic_score
 }
 
 pub fn run() {
     let input = fs::read_to_string("src/inputs/day8").unwrap();
 
-    let grid: Vec<Vec<u32>> = input
+    let width = input.lines().last().unwrap().len();
+    let height = input.lines().count();
+
+    let grid: Vec<Vec<u8>> = input
         .lines()
-        .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
+        .map(|line| line.chars().map(|h| (h as u8 - b'0') as u8).collect())
         .collect();
 
-    let visible_trees = count_visible_trees(&grid);
-    let best_scenic_view = best_scenic_view(&grid);
+    let visible_trees = count_visible_trees_refactored(&grid, &height, &width);
+    let scenic_score = scenic_score_refactored(&grid, &height, &width);
 
     println!("Visible trees: {}", visible_trees);
-    println!("Scenic view: {}", best_scenic_view);
-
-    // println!("part1: {}", test.0); println!("part2: {}", test.1)
+    println!("Scenic score: {}", scenic_score);
 }
--- RUST/src/main.rs
+++ RUST/src/main.rs
@@ -1,4 +1,5 @@
 mod days;
 fn main() {
+    days::day7::run();
     days::day8::run()
 }
